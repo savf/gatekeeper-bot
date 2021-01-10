0f44d3edfdd853f715dbf16c9317e0b027130fdd
Stephan Mannhart <stephan.mannhart@googlemail.com>
2021-01-10 19:47:24 +0100

    Added loguru, updated telegram bot api

--- main.py
+++ main.py
@@ -16,24 +16,25 @@ Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 """
 import os
-import logging
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from loguru import logger
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ChatPermissions
 from telegram.ext import Filters, Updater, MessageHandler, CommandHandler, CallbackQueryHandler
+from telegram.error import BadRequest
 from random import shuffle
 
 # Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
-logger = logging.getLogger(__name__)
+logger.add("logs/gatekeeper-bot_{time}.log",
+           format="{time} - {name} - {level} - {message}", level="INFO", rotation="5 MB")
 
 # Define a few command handlers. These usually take the two arguments bot and
 # update. Error handlers also receive the raised TelegramError object in error.
+
+
 def start(update, context):
     """Send a message when the command /start is issued."""
-    update.message.reply_text("Hello, I am the Swiss Mechanical Keyboard Enthusiasts Gatekeeper. " \
-                "As soon as a new user joins, they will be restricted to keep them " \
-                "from posting until they prove they are not a robot.")
+    update.message.reply_text("Hello, I am the Swiss Mechanical Keyboard Enthusiasts Gatekeeper. "
+                              "As soon as a new user joins, they will be restricted to keep them "
+                              "from posting until they prove they are not a robot.")
 
 
 def help(update, context):
@@ -46,6 +47,33 @@ def id(update, context):
     update.message.reply_text(update.effective_chat.id)
 
 
+def remove_job_if_exists(name, context):
+    """Remove job with given name. Returns whether job was removed."""
+    current_jobs = context.job_queue.get_jobs_by_name(str(name))
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+
+def welcome_timeout(context):
+    """Remove welcome message after predifined timeout in os.environ['TIMEOUT'] """
+    job_context = context.job.context
+
+    if 'message_id' in job_context:
+        try:
+            context.bot.delete_message(
+                chat_id=os.environ['CHAT_ID'],
+                message_id=job_context['message_id']
+            )
+            logger.info('Welcome message for "{first_name}" timed out after {seconds} seconds.',
+                        first_name=job_context['first_name'], seconds=os.environ['TIMEOUT'])
+        except BadRequest:
+            logger.info('Welcome message with id "{message_id}" no longer exists.',
+                        message_id=job_context['message_id'])
+
+
 def hodor(update, context):
     try:
         for new_member in update.message.new_chat_members:
@@ -53,32 +81,42 @@ def hodor(update, context):
             context.bot.restrict_chat_member(
                 int(os.environ['CHAT_ID']),
                 new_member.id,
-                can_send_messages=False,
-                can_send_media_messages=False,
-                can_send_other_messages=False,
-                can_add_web_page_previews=False
+                ChatPermissions(
+                    can_send_messages=False,
+                    can_send_media_messages=False,
+                    can_send_other_messages=False,
+                    can_add_web_page_previews=False)
             )
 
             keyboard_items = [
-                InlineKeyboardButton("Cherry MX", callback_data=callback_id + ',dream'),
-                InlineKeyboardButton("Topre", callback_data=callback_id + ',of'),
-                InlineKeyboardButton("Buckling Spring", callback_data=callback_id + ',electric'),
-                InlineKeyboardButton("Alps", callback_data=callback_id + ',sheep')
+                InlineKeyboardButton(
+                    'Mechanical', callback_data=callback_id + ',mech'),
+                InlineKeyboardButton(
+                    'Rubberdome', callback_data=callback_id + ',rubber'),
             ]
 
             shuffle(keyboard_items)
             keyboard = []
-            for i in range(4):
+            for i in range(2):
                 keyboard.append([keyboard_items[i]])
             reply_markup = InlineKeyboardMarkup(keyboard)
 
-            update.message.reply_text(
+            timeout_seconds = int(os.environ['TIMEOUT'])
+            timeout_minutes = str(int(timeout_seconds / 60))
+            message = update.message.reply_text(
                 'Hello, ' +
                 new_member.first_name +
-                ' and welcome to the Swiss Mechanical Keyboard Enthusiasts Telegram group. Just a small formality before we allow you to post: Please prove that you are not a robot by choosing "Cherry MX" below. Thank you.',
+                ' and welcome to the Swiss Mech Chat. Just a small formality before we allow you to post: ' +
+                'Please prove that you are not a robot by choosing "Mechanical" below. You have ' +
+                timeout_minutes +
+                ' minutes to answer.',
                 reply_markup=reply_markup,
                 disable_notification=True
             )
+
+            remove_job_if_exists(callback_id, context)
+            context.job_queue.run_once(welcome_timeout, timeout_seconds, context={
+                                       "message_id": message.message_id, "first_name": new_member.first_name}, name=callback_id)
     except AttributeError:
         pass
 
@@ -86,11 +124,12 @@ def hodor(update, context):
 def button(update, context):
     query = update.callback_query
     person_who_pushed_the_button = int(query.data.split(",")[0])
-    print("Query user: " + str(query.from_user))
-    print("Query data: " + str(query.data))
 
     if query.from_user.id == person_who_pushed_the_button:
-        if 'dream' in query.data:
+        logger.info('User "{user}" sent back "{reply}"',
+                    user=str(query.from_user), reply=str(query.data))
+        remove_job_if_exists(person_who_pushed_the_button, context)
+        if 'mech' in query.data:
             context.bot.delete_message(
                 chat_id=update.callback_query.message.chat_id,
                 message_id=update.callback_query.message.message_id
@@ -98,10 +137,11 @@ def button(update, context):
             context.bot.restrict_chat_member(
                 int(os.environ['CHAT_ID']),
                 person_who_pushed_the_button,
-                can_send_messages=True,
-                can_send_media_messages=True,
-                can_send_other_messages=True,
-                can_add_web_page_previews=True
+                ChatPermissions(
+                    can_send_messages=True,
+                    can_send_media_messages=True,
+                    can_send_other_messages=True,
+                    can_add_web_page_previews=True)
             )
         else:
             query.edit_message_text(text="🚨 Looks like we have another bot. 🚨")
@@ -109,7 +149,8 @@ def button(update, context):
 
 def error(update, context):
     """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
+    logger.warning('Update "{update}" caused error "{error}"',
+                   update=update, error=context.error)
 
 
 def main():
@@ -126,7 +167,8 @@ def main():
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("id", id))
 
-    dp.add_handler(MessageHandler(Filters.chat(int(os.environ['CHAT_ID'])), hodor))
+    dp.add_handler(MessageHandler(
+        Filters.chat(int(os.environ['CHAT_ID'])), hodor))
 
     updater.dispatcher.add_handler(CallbackQueryHandler(button))
 
